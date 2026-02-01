f4fd48c114af65d9ffd69307d28feb596a7d5587
Bikal Koju <bikalkoju123@gmail.com>
2026-02-01 18:28:16 +0545

    word and input fields added

--- hangman/index.js
+++ hangman/index.js
@@ -1,3 +1,7 @@
+const words = ['Naruto', 'Attack on Titan', 'My Hero Academia', 'One Piece'];
+let selectedWord = null;
+
+
 hangman();
 
 function hangman() {
@@ -8,12 +12,13 @@ function hangman() {
     const [ctx, cw, ch] = canvasSetup( hangmanCanvas );
 
     drawPole( ctx, cw, ch );
-    drawHead( ctx, cw, ch );
-    drawBody( ctx, cw, ch );
-    drawLeftHand( ctx, cw, ch );
-    drawRightHand( ctx, cw, ch );
-    drawLeftLeg( ctx, cw, ch );
-    drawRightLeg( ctx, cw, ch );
+    generateWord();
+    // drawHead( ctx, cw, ch );
+    // drawBody( ctx, cw, ch );
+    // drawLeftHand( ctx, cw, ch );
+    // drawRightHand( ctx, cw, ch );
+    // drawLeftLeg( ctx, cw, ch );
+    // drawRightLeg( ctx, cw, ch );
 }
 
 function canvasSetup( canvas ) {
@@ -21,7 +26,7 @@ function canvasSetup( canvas ) {
     const cw = canvas.width = canvasWrapperHeight / 1.5;
     const ch = canvas.height = canvasWrapperHeight;
 
-    canvas.style.border = '1px solid #000';
+    // canvas.style.border = '1px solid #000';
 
     const ctx = canvas.getContext( '2d' );
 
@@ -68,6 +73,7 @@ function drawLeftHand( ctx, cw, ch ) {
     ctx.strokeStyle = '#000';
     ctx.stroke();
 }
+
 function drawRightHand( ctx, cw, ch ) { 
     ctx.beginPath();
     ctx.moveTo( cw / 2, ( ch / 100 ) * 20 + ( 2 * ( ( cw / 100 ) * 15 ) ) );
@@ -77,6 +83,7 @@ function drawRightHand( ctx, cw, ch ) {
     ctx.strokeStyle = '#000';
     ctx.stroke();
 }
+
 function drawLeftLeg( ctx, cw, ch ) { 
     ctx.beginPath();
     ctx.moveTo( cw / 2, ( ch / 100 ) * 70 );
@@ -86,6 +93,7 @@ function drawLeftLeg( ctx, cw, ch ) {
     ctx.strokeStyle = '#000';
     ctx.stroke();
 }
+
 function drawRightLeg( ctx, cw, ch ) { 
     ctx.beginPath();
     ctx.moveTo( cw / 2, ( ch / 100 ) * 70 );
@@ -95,3 +103,35 @@ function drawRightLeg( ctx, cw, ch ) {
     ctx.strokeStyle = '#000';
     ctx.stroke();
 }
+
+function generateWord() {
+    const randomIndex = Math.floor( Math.random() * words.length );
+    selectedWord = words[randomIndex];
+
+    generateInputBoxes( selectedWord );
+}
+
+function generateInputBoxes( selectedWord ) {
+    const letters = selectedWord.split( '' );
+    const wordsInputWrapper = document.querySelector( '.words_input' );
+
+    if ( ! wordsInputWrapper ) return;
+
+    letters.forEach( ( letter, index ) => {
+        if ( /^[a-z]$/i.test( letter ) ) {
+            const input = document.createElement( 'input' );
+            input.setAttribute( 'name', `letter_${index}` );
+            wordsInputWrapper.appendChild( input );
+        } else if ( letter === ' ' ) {
+            const space = document.createElement( 'div' );
+            space.className = 'space';
+            wordsInputWrapper.appendChild( space );
+        } 
+        else {
+            const letter = document.createElement( 'div' );
+            wordsInputWrapper.appendChild( letter );
+        }
+    } )
+    console.log(letters)
+}   
+
